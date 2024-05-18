d4240bb6c2e1d2a0ebbb45b74e051c713a25a92c
Anton Rudenok <arudenok@gmail.com>
2024-05-18 17:09:02 +0300

    Use CameraWindowState for set_active_camera instead

--- crates/bevy_editor_pls_default_windows/src/cameras/camera_window_ext.rs
+++ crates/bevy_editor_pls_default_windows/src/cameras/camera_window_ext.rs
@@ -1,25 +1,17 @@
-use bevy::log::info;
 use bevy::prelude::World;
-use bevy_editor_pls_core::Editor;
-use crate::cameras::{CameraWindow, EditorCamKind, set_active_editor_camera_marker};
+use crate::cameras::{CameraWindowState, EditorCamKind, set_active_editor_camera_marker};
 
-impl CameraWindow {
+impl CameraWindowState {
 	/// Sets current active editor camera to the specified `EditorCamKind`
-	pub fn set_active_camera(&self,
+	pub fn set_active_camera(&mut self,
 							 camera: EditorCamKind,
 							 world: &mut World,
-							 editor: &mut Editor,
 	)
 	{
-		let Some(state) = editor.window_state_mut::<CameraWindow>() else {
-			info!("Unable to fetch CameraWindowState. Check if CameraWindow is available");
-			return;
-		};
-
-		if state.editor_cam != camera {
+		if self.editor_cam != camera {
 			set_active_editor_camera_marker(world, camera);
 		}
 
-		state.editor_cam = camera;
+		self.editor_cam = camera;
 	}
 }
